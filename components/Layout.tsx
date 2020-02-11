d774a6d0c2b952c2cfbac1e8ae8ec82f0c1b20ee
Michael <mikedev0431@gmail.com>
2020-02-10 16:44:38 -0800

    Added Layout hoc New hoc component was created "Layout.tsx". This will be used as one layout the rest of our website moving forward.

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout: React.SFC = ({ chidren }: any) => {
+    return (
+        <div>
+            {chidren}
+        </div>
+    )
+}
+
+export default Layout;
