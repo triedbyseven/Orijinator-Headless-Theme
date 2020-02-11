604a00dd2bac0d6514ace42fcd26713f28bfbd7b
Michael <mikedev0431@gmail.com>
2020-02-10 16:13:39 -0800

    New directory added Blog directory was created to better sort out all of our compents that will work render our blog pages.

---
+++ pages/blog/[slug].js
@@ -0,0 +1,10 @@
+const Slug = () => {
+    return (
+        <div>
+            <h1>Slug Title</h1>
+            <p>More data will go here or content.</p>
+        </div>
+    )
+};
+
+export default Slug;
