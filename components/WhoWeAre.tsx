54da6b5f191a95b025c08c326eb1946611a0829b
triedbyseven <mikedev0431@gmail.com>
2020-02-18 06:46:23 -0800

    Updated to render view Now renders a container from uikit.

---
+++ components/WhoWeAre.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export interface WhoWeAreProps {}
+
+const WhoWeAre: React.SFC<WhoWeAreProps> = () => {
+  return <div className="container"></div>;
+};
+
+export default WhoWeAre;
