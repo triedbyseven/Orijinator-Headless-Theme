82aa72bdf3a93801d9e0bc25e27d95c3d25c6b21
triedbyseven <mikedev0431@gmail.com>
2020-02-18 06:40:50 -0800

    Added UI kit initialization. UI kit css was commented out for some reason.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import 'uikit/dist/css/uikit.min.css'
+import 'uikit/dist/css/uikit.min.css';
 
 const Layout: React.SFC = ({ children }: any) => {
-    return (
-        <div className="uk-container">
-            {children}
-        </div>
-    )
-}
+  return (
+    <div className="uk-container" style={{ maxWidth: '100%', padding: 0 }}>
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
