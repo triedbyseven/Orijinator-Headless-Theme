527b0edecad20519134bcf499f3fb748fd08e1af
triedbyseven <mikedev0431@gmail.com>
2020-02-21 13:34:20 -0800

    Added meta tags and script tags All script and meta tags and scripts have been added verus imported here.
    Using them in this fashion allows google fonts and the  UI Kit to be available to the entire app when the page is finally loaded from the server.

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/uikit@3.3.2/dist/css/uikit.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.2/dist/js/uikit.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.2/dist/js/uikit-icons.min.js"></script>
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
